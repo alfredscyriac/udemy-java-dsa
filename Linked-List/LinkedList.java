d7edfefd72b4f7c05be0f2bdc56f483130b0db1d
alfredscyriac <alfredscyriac@gmail.com>
2024-12-18 20:28:37 -0500

    LinkedList class with constructor

---
+++ Linked-List/LinkedList.java
@@ -0,0 +1,11 @@
+public class LinkedList {
+    private ListNode head; 
+    private ListNode tail; 
+    private int length;
+    public LinkedList(int value){
+        ListNode newNode = new ListNode(value);
+        head=newNode;
+        tail=newNode;
+        length+=1; 
+    }    
+}
