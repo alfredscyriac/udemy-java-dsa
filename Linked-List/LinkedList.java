c464b87214298e7603e861f9dc6ff16ec3934170
alfredscyriac <alfredscyriac@gmail.com>
2024-12-18 20:36:39 -0500

    Create PrintList method for LinkedList class

--- Linked-List/LinkedList.java
+++ Linked-List/LinkedList.java
@@ -1,11 +1,23 @@
 public class LinkedList {
+    // Private instance variables 
     private ListNode head; 
     private ListNode tail; 
     private int length;
+
+    // Linked List Constructor 
     public LinkedList(int value){
         ListNode newNode = new ListNode(value);
         head=newNode;
         tail=newNode;
         length=1; 
-    }    
+    } 
+
+    // Method that prints the entire linked list
+    public void PrintList(){
+        ListNode current = head; 
+        while(current.next!=null){
+            System.out.println(current.value);
+            current=current.next; 
+        }
+    }   
 }
